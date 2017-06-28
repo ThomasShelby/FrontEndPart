52fd072cf91c0f881b8f27c5e50fbfc8e0618b7c
volodimyr <l.volodimyr@gmail.com>
2017-06-28 16:56:57 +0300

    Added api.js as a lib for sending requests.

--- utils/api.js
+++ utils/api.js
@@ -1,2 +1,14 @@
 var axios = require('axios');
 
+var getDevelopersURL = "https://api.myjson.com/bins/9xu4z";
+
+module.exports = {
+    getDevelopers: function () {
+        var encodedURI = window.encodeURI(getDevelopersURL);
+        return axios.get(encodedURI)
+            .then(function (response) {
+                console.log('API: ' + response.data.developers);
+                return response.data;
+            });
+    }
+};
