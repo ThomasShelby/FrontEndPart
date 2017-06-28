99cac311219b24c95d43479f75ce4df0e9dcb37f
volodimyr <l.volodimyr@gmail.com>
2017-06-28 14:56:57 +0300

    Created react self-structured project. Updated .gitignore. Created webpack.config.js. Added common dependencies.

---
+++ app/index.js
@@ -0,0 +1,22 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+require('./index.css');
+
+//state
+//lifecycle
+//UI
+
+class App extends React.Component {
+    render() {
+        return (
+            <div>
+                Hello world!
+            </div>
+        )
+    }
+}
+
+ReactDOM.render(
+    <App/>,
+    document.getElementById('app')
+);
