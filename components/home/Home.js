2f90abf14125a14c1fe7e9eccbad35875fa9cae8
volodimyr <l.volodimyr@gmail.com>
2017-07-08 15:33:28 +0300

    Created new component for Header and added new styles

--- components/home/Home.js
+++ components/home/Home.js
@@ -1,12 +1,16 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
 var Carousel = require('./carousel/Carousel');
+var Header = require('./header/Header');
 
 
 class Home extends React.Component {
     render() {
         return (
-            <Carousel/>
+            <div>
+                <Carousel/>
+                <Header/>
+            </div>
         )
     }
 }
